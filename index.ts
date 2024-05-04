ae794a544b6179a9dd0ec3525c877745b4d48890
confh <fares.aiman@gmail.com>
2024-05-04 13:07:21 +0300

    Automated commit

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import Client from "./DiscordApiWrapper/Client";
+import { SlashCommandInteraction } from "./DiscordApiWrapper/classes/Interaction";
+import SlashCommandBuilder from "./DiscordApiWrapper/classes/SlashCommandBuilder";
+require('dotenv').config()
+
+const client = new Client(process.env.TOKEN as string, {
+    cacheAllUsers: true
+})
+
+client.on("ready", () => {
+    console.log("ready")
+    client.setGlobalCommands([
+        new SlashCommandBuilder()
+            .setName("ping")
+            .setDescription("Ping the bot.")
+    ])
+})
+
+client.on("interactionCreate", (i) => {
+    if (i instanceof SlashCommandInteraction) {
+        if (i.name === "ping") {
+            i.reply("Pong!")
+        }
+    }
+})
+
+client.connect()
