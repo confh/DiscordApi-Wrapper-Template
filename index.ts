b645e9743cf59897f1500a61d428f22b607890ad
confh <fares.aiman@gmail.com>
2024-05-04 13:43:50 +0300

    Automated commit

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import Client from "./DiscordApiWrapper/Client";
+import EmbedBuilder from "./DiscordApiWrapper/classes/EmbedBuilder";
 import { SlashCommandInteraction } from "./DiscordApiWrapper/classes/Interaction";
 import SlashCommandBuilder from "./DiscordApiWrapper/classes/SlashCommandBuilder";
 require('dotenv').config()
@@ -9,17 +10,33 @@ const client = new Client(process.env.TOKEN as string, {
 
 client.on("ready", () => {
     console.log("ready")
-    client.setGlobalCommands([
+    client.setGlobalCommands(
         new SlashCommandBuilder()
             .setName("ping")
             .setDescription("Ping the bot.")
-    ])
+    )
 })
 
-client.on("interactionCreate", (i) => {
+client.on("interactionCreate", async (i) => {
     if (i instanceof SlashCommandInteraction) {
         if (i.name === "ping") {
-            i.reply("Pong!")
+            const msg = await i.channel.send("Calculating ping...")
+            const ping = Math.abs(msg.timestamp - Date.now())
+            await msg.delete()
+
+            const embed = new EmbedBuilder()
+                .setTitle("Poing")
+                .setDescription(`**Ping:** ${ping}ms`)
+                .setFooter({
+                    text: client.user.displayName as string,
+                    icon_url: client.user.getAvatarURL({ size: 1024 })
+                })
+                .setTimestamp()
+
+            await i.reply({
+                content: "",
+                embeds: [embed],
+            })
         }
     }
 })
